462457cf0424c5dc0e0daddd66a0ca294be27081
pealbert <pealbert@email.cz>
2026-08-14 16:50:28 +0200

    Fixed: Install remixicon and biome packages. Run format and lint

--- src/js/main.js
+++ src/js/main.js
@@ -1,34 +1,38 @@
 /*~~~~~~~~~~~~~~~ IMPORTS ~~~~~~~~~~~~~~~*/
-import Swiper from 'swiper/bundle';
-import ScrollReveal from 'scrollreveal';
+import Swiper from "swiper/bundle";
+import "swiper/css/bundle";
+
+import ScrollReveal from "scrollreveal";
+
+import "remixicon/fonts/remixicon.css";
 
 /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
-const navMenu = document.getElementById('nav-menu');
-const navLink = document.querySelectorAll('.nav-link');
-const hamburger = document.getElementById('hamburger');
+const navMenu = document.getElementById("nav-menu");
+const navLink = document.querySelectorAll(".nav-link");
+const hamburger = document.getElementById("hamburger");
 
-hamburger.addEventListener('click', () => {
-	navMenu.classList.toggle('left-[0]');
-	hamburger.classList.toggle('ri-close-large-line');
+hamburger.addEventListener("click", () => {
+	navMenu.classList.toggle("left-[0]");
+	hamburger.classList.toggle("ri-close-large-line");
 });
 
-navLink.forEach(link => {
+navLink.forEach((link) => {
 	link.addEventListener("click", () => {
-		navMenu.classList.toggle('left-[0]');
-		hamburger.classList.toggle('ri-close-large-line');
+		navMenu.classList.toggle("left-[0]");
+		hamburger.classList.toggle("ri-close-large-line");
 	});
 });
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
 const scrollUp = () => {
-	const scrollUpBtn = document.getElementById('scroll-up');
+	const scrollUpBtn = document.getElementById("scroll-up");
 
 	if (window.scrollY >= 250) {
-		scrollUpBtn.classList.remove('-bottom-1/2');
-		scrollUpBtn.classList.add('bottom-4');
+		scrollUpBtn.classList.remove("-bottom-1/2");
+		scrollUpBtn.classList.add("bottom-4");
 	} else {
-		scrollUpBtn.classList.remove('bottom-4');
-		scrollUpBtn.classList.add('-bottom-1/2');
+		scrollUpBtn.classList.remove("bottom-4");
+		scrollUpBtn.classList.add("-bottom-1/2");
 	}
 };
 
@@ -36,67 +40,66 @@ window.addEventListener("scroll", scrollUp);
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
 const scrollHeader = () => {
-	const header = document.getElementById('navbar');
+	const header = document.getElementById("navbar");
 
 	if (window.scrollY >= 50) {
-		header.classList.add('border-b', 'border-yellow-500');
+		header.classList.add("border-b", "border-yellow-500");
 	} else {
-		header.classList.remove('border-b', 'border-yellow-500');
+		header.classList.remove("border-b", "border-yellow-500");
 	}
 };
 
 window.addEventListener("scroll", scrollHeader);
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
-const swiper = new Swiper('.swiper', {
+const _swiper = new Swiper(".swiper", {
 	speed: 400,
 	spaceBetween: 30,
 	autoplay: {
 		delay: 3000,
-		disableOnInteraction: false
+		disableOnInteraction: false,
 	},
 
 	pagination: {
-		el: '.swiper-pagination',
-		clickable: true
+		el: ".swiper-pagination",
+		clickable: true,
 	},
 	grabCursor: true,
 	breakpoints: {
 		640: {
-			slidesPerView: 1
+			slidesPerView: 1,
 		},
 		768: {
-			slidesPerView: 2
+			slidesPerView: 2,
 		},
 		1024: {
-			slidesPerView: 3
+			slidesPerView: 3,
 		},
-	}
+	},
 });
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
 const activeLink = () => {
-	const sections = document.querySelectorAll('section');
-	const navLinks = document.querySelectorAll('.nav-link');
+	const sections = document.querySelectorAll("section");
+	const navLinks = document.querySelectorAll(".nav-link");
 
 	let current = "home";
 
-	sections.forEach(section => {
+	sections.forEach((section) => {
 		const sectionTop = section.offsetTop;
 
 		if (window.scrollY >= sectionTop - 60) {
-			current = section.getAttribute('id');
+			current = section.getAttribute("id");
 		}
 	});
 
-	navLinks.forEach(item => {
-		item.classList.remove('active');
+	navLinks.forEach((item) => {
+		item.classList.remove("active");
 
 		if (item.href.includes(current)) {
-			item.classList.add('active');
-
+			item.classList.add("active");
 		}
-	})
+	});
 };
 
 window.addEventListener("scroll", activeLink);
@@ -107,16 +110,18 @@ const sr = ScrollReveal({
 	distance: "60px",
 	duration: 2500,
 	delay: 300,
-	reset: true
+	reset: true,
 });
 
-sr.reveal(`.home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, .footer__content, .copy__right`)
-sr.reveal(`.home__image`, {delay: 500, scale: 0.5})
+sr.reveal(
+	`.home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, .footer__content, .copy__right`,
+);
+sr.reveal(`.home__image`, { delay: 500, scale: 0.5 });
 
-sr.reveal(`.service__card, .popular__card`, {interval: 100})
+sr.reveal(`.service__card, .popular__card`, { interval: 100 });
 
-sr.reveal(`.about__leaf`, {delay: 1000, origin: "right"})
-sr.reveal(`.about__item__1-content, .about__item__2-img`, {origin: "right"})
-sr.reveal(`.about__item__2-content, .about__item__1-img`, {origin: "left"})
+sr.reveal(`.about__leaf`, { delay: 1000, origin: "right" });
+sr.reveal(`.about__item__1-content, .about__item__2-img`, { origin: "right" });
+sr.reveal(`.about__item__2-content, .about__item__1-img`, { origin: "left" });
 
-sr.reveal(`.review__leaf, .footer__floral`, {delay: 1000, origin: "left"})
+sr.reveal(`.review__leaf, .footer__floral`, { delay: 1000, origin: "left" });
