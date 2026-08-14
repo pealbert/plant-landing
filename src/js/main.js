95d17bbd8f68f9a3d9b41e26bc955724764c570c
pealbert <pealbert@email.cz>
2026-08-14 16:17:34 +0200

    Animate selected items using scrollreveal. Fixed: Install Swiper package instead of CDN

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+/*~~~~~~~~~~~~~~~ IMPORTS ~~~~~~~~~~~~~~~*/
+import Swiper from 'swiper/bundle';
+import ScrollReveal from 'scrollreveal';
+
 /*~~~~~~~~~~~~~~~ TOGGLE BUTTON ~~~~~~~~~~~~~~~*/
 const navMenu = document.getElementById('nav-menu');
 const navLink = document.querySelectorAll('.nav-link');
@@ -16,10 +20,32 @@ navLink.forEach(link => {
 });
 
 /*~~~~~~~~~~~~~~~ SHOW SCROLL UP ~~~~~~~~~~~~~~~*/
+const scrollUp = () => {
+	const scrollUpBtn = document.getElementById('scroll-up');
 
+	if (window.scrollY >= 250) {
+		scrollUpBtn.classList.remove('-bottom-1/2');
+		scrollUpBtn.classList.add('bottom-4');
+	} else {
+		scrollUpBtn.classList.remove('bottom-4');
+		scrollUpBtn.classList.add('-bottom-1/2');
+	}
+};
+
+window.addEventListener("scroll", scrollUp);
 
 /*~~~~~~~~~~~~~~~ CHANGE BACKGROUND HEADER ~~~~~~~~~~~~~~~*/
+const scrollHeader = () => {
+	const header = document.getElementById('navbar');
 
+	if (window.scrollY >= 50) {
+		header.classList.add('border-b', 'border-yellow-500');
+	} else {
+		header.classList.remove('border-b', 'border-yellow-500');
+	}
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 /*~~~~~~~~~~~~~~~ SWIPER ~~~~~~~~~~~~~~~*/
 const swiper = new Swiper('.swiper', {
@@ -49,6 +75,48 @@ const swiper = new Swiper('.swiper', {
 });
 
 /*~~~~~~~~~~~~~~~ SCROLL SECTIONS ACTIVE LINK ~~~~~~~~~~~~~~~*/
+const activeLink = () => {
+	const sections = document.querySelectorAll('section');
+	const navLinks = document.querySelectorAll('.nav-link');
 
+	let current = "home";
+
+	sections.forEach(section => {
+		const sectionTop = section.offsetTop;
+
+		if (window.scrollY >= sectionTop - 60) {
+			current = section.getAttribute('id');
+		}
+	});
+
+	navLinks.forEach(item => {
+		item.classList.remove('active');
+
+		if (item.href.includes(current)) {
+			item.classList.add('active');
+
+		}
+	})
+};
+
+window.addEventListener("scroll", activeLink);
 
 /*~~~~~~~~~~~~~~~ SCROLL REVEAL ANIMATION ~~~~~~~~~~~~~~~*/
+const sr = ScrollReveal({
+	origin: "top",
+	distance: "60px",
+	duration: 2500,
+	delay: 300,
+	reset: true
+});
+
+sr.reveal(`.home__data, .about__top, .popular__top, .review__top, .review__swiper, .footer__icon, .footer__content, .copy__right`)
+sr.reveal(`.home__image`, {delay: 500, scale: 0.5})
+
+sr.reveal(`.service__card, .popular__card`, {interval: 100})
+
+sr.reveal(`.about__leaf`, {delay: 1000, origin: "right"})
+sr.reveal(`.about__item__1-content, .about__item__2-img`, {origin: "right"})
+sr.reveal(`.about__item__2-content, .about__item__1-img`, {origin: "left"})
+
+sr.reveal(`.review__leaf, .footer__floral`, {delay: 1000, origin: "left"})
